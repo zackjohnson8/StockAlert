0cc556d81919298ef228a57113ebc513a41380f1
Zachary Johnson <zackjohnson8@gmail.com>
2022-09-26 07:52:28 -0700

    Changed - Upper hstack to a vstack for graph placement instead of stock icons. Height set at 30% to fix any sizing issues that might occur on other devices.

--- StockAlert/ContentView.swift
+++ StockAlert/ContentView.swift
@@ -16,45 +16,54 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-
-                HStack(alignment: .top) {
-                    Text("HStack")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(.leading)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .border(.black, width: 2)
-                }
-                .frame(maxWidth: .infinity, alignment: .center)
-                .border(.black, width: 2)
-
-                Spacer()
-
+            GeometryReader { metrics in
                 VStack(alignment: .leading) {
-                    Spacer()
-                    Text("VStack")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(.leading)
-                            .frame(maxWidth: .infinity, alignment: .top)
+
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        Text("VStack")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(.leading)
+                                .frame(maxWidth: .infinity, alignment: .top)
+                                .border(.black, width: 2)
+                        Spacer()
+                    }
+                            .frame(
+                                    minWidth: 0,
+                                    maxWidth: .infinity,
+                                    minHeight: 0,
+                                    maxHeight: metrics.size.height * 0.30,
+                                    alignment: .topLeading
+                            )
                             .border(.black, width: 2)
+
                     Spacer()
+
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        Text("VStack")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(.leading)
+                                .frame(maxWidth: .infinity, alignment: .top)
+                                .border(.black, width: 2)
+                        Spacer()
+                    }
+                            .frame(
+                                    minWidth: 0,
+                                    maxWidth: .infinity,
+                                    minHeight: 0,
+                                    maxHeight: .infinity,
+                                    alignment: .topLeading
+                            )
+                            .border(.black, width: 2)
+
+
                 }
-                .frame(
-                        minWidth: 0,
-                        maxWidth: .infinity,
-                        minHeight: 0,
-                        maxHeight: .infinity,
-                        alignment: .topLeading
-                )
-                .border(.black, width: 2)
-
-
+                        .navigationTitle(Text("Nav Text").font(.subheadline))
+                        .border(.black, width: 2)
             }
-            .navigationTitle(Text("Nav Text").font(.subheadline))
-            .border(.black, width: 2)
-
         }
         .border(.black, width: 2)
     }
